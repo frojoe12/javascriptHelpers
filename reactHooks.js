154f6a2c2d81fe07048e358e8b766dc8d7e33757
frojoe12 <45601644+frojoe12@users.noreply.github.com>
2021-08-27 10:26:58 +0100

    Update reactHooks.js

--- reactHooks.js
+++ reactHooks.js
@@ -1,1 +1,52 @@
+import React from "react"
+import { useState, useEffect, useCallback, useMemo } from "react"
 
+/*
+always use the setter for useState
+always put a dependency array on useEffect, useCallback, useMemo
+to run useEgged only once use an empty array
+don't depend on data you set
+always add the state you read from to the dependency array
+*/
+
+function MyComponent() {
+    const [numbers, setNumbers] = useState([]);
+
+    useEffect(()=>{
+        fetch("/numbers.json")
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("data then start")
+                    setNumbers(data);
+                    console.log(data)
+                })
+                .catch((error)=>{
+                    console.log(error)
+                })
+    },[])
+
+    const addOne = useCallback(() => {
+        setNumbers(currentNumbers=>[...currentNumbers, currentNumbers.length+1])
+    },[]);
+
+    const sum = useMemo(()=>(numbers.reduce((a,v) => a+v,0)),[numbers])
+    
+    const out =  (
+        <div>
+        <div>{sum}</div>
+        <div>Numbers {numbers.map(number => `${number}, `)}</div>
+                    <button onClick={addOne}>Add One</button>
+        </div>
+    )    
+    return out;
+}
+
+const App = () => {
+    return (
+        <div>
+            <MyComponent />
+        </div>
+    )
+}
+
+export default App
