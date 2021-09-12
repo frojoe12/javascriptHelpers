94ee48bd2636e4338e034153e519b3dc1a586ed2
frojoe12 <45601644+frojoe12@users.noreply.github.com>
2021-09-12 14:39:46 +0100

    Update async.js

--- async/async.js
+++ async/async.js
@@ -1,1 +1,32 @@
+console.log("loaded")
 
+const apiUrl="http://universities.hipolabs.com"
+
+const render = (content = "...") => {
+    document.getElementById("root").innerHTML=content;
+}
+const debug = (content = "...") => {
+    document.getElementById("debug").innerHTML=content;
+}
+
+render("loading...");
+debug("ready")
+
+const getData = async () => {
+    try{
+    await fetch(apiUrl)
+        .then(res=>res.json())
+        .then(data=>render(data.example))
+    } 
+    catch(err){
+        console.error(err)
+        render("something went wrong...")
+    }
+    finally {
+        debug("finished execution")
+    }
+}
+
+getData();
+
+console.log("end")
