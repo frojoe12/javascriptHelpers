e2830c3e1080ee2fb772f8a95a98ec860875372d
frojoe12 <45601644+frojoe12@users.noreply.github.com>
2022-01-18 18:27:24 +0000

    Create promise.js
    first commit

---
+++ promise.js
@@ -0,0 +1,32 @@
+const main = 
+  [
+    {id:1,name:"John"}, 
+    {id:2,name:"James"},
+    {id:3,name:"Jim"}
+  ]
+
+const app = () => {
+  
+}
+
+let fetchSomeData = New promise((resolve, reject) => {
+  if (!allGood) {
+    reject("error")
+  } else {
+    resolve {
+      id:1,
+        message: "complete
+
+    });
+
+mainData.fetch(response => {
+}).then(data => {
+  console.log(data)
+})
+    
+    
+fetchSomeData.then(fetchedData => {
+  console.log('then', fetchData)
+}).catch(err => {
+  console.error('catch',err);
+})
